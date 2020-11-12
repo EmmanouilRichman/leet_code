feb9fa1ad7050fd79d401afff87efd9432db43db
Emmanouil Richman <erichman966@gmail.com>
2020-11-11 22:34:44 -0500

    implement best time to buy and sell

---
+++ src/com/emmanouil/buy_sell.java
@@ -0,0 +1,20 @@
+package com.emmanouil;
+
+public class buy_sell {
+
+    public static int maxProfit(int[] prices){
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        int n = prices.length;
+
+        for(int i = 0; i < n; i++){
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            else if(prices[i] - minPrice > maxProfit){
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
